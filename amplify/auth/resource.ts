7c6dad701bf7dd195851aa9b948a60452820d524
Yara Abi Raad <101484139+yarah123@users.noreply.github.com>
2025-07-02 02:13:30 +0300

    Update resource.ts

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,6 +1,5 @@
 import { defineAuth } from '@aws-amplify/backend';
 import { postConfirmation } from './post-confirmation/resource';
-
 export const auth = defineAuth({
   loginWith: {
     email: true,
